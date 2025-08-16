37a6c75bd14cb920ca94d13a3fcbb15a4c582e44
Pratiksha Chougule <pratiksha42719@gmail.com>
2025-08-16 15:15:49 +0530

    moving zeros at the end

---
+++ movezeros.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int j = -1;
+
+        // Find the first zero
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        // If there are no zeroes, we're done
+        if (j == -1) return;
+
+        // Move non-zero elements forward
+        for (int i = j + 1; i < n; i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
