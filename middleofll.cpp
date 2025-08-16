49f2f07ddb78893ad115bedd62211323c96e91f0
Pratiksha Chougule <chougulepratiksha23@gamil.com>
2025-08-16 14:26:57 +0530

    midle of linied list

---
+++ middleofll.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct node {
+ *     int val;
+ *     node *next;
+ *     node() : val(0), next(nullptr) {}
+ *    node(int x) : val(x), next(nullptr) {}
+ *   node(int x, node*next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    node* middleofll(node* head) {
+  if(head==NULL || head->next==NULL)
+  return head;
+  node* slow=head;
+  node* fast=head;
+  node* curr=head;
+  while(curr->next!=NULL)
+  {
+    slow=slow->next;
+    fast=fast->next->next;
+
+  }  
+  return slow;  
+    }
+};
