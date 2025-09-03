4e164dcec29f7d8d101062e1cabddaa71c62bf7a
Pratiksha Chougule <pratiksha42719@gmail.com>
2025-09-03 09:08:44 +0530

    min diff in heights after inc or dec by k

---
+++ mindiffinht.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int getMinDiff(vector<int> &arr, int k) {
+         sort(arr, arr + n);
+
+    int ans = arr[n - 1] - arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] - k < 0) continue; 
+
+        int minHeight = min(arr[0] + k, arr[i] - k);
+        int maxHeight = max(arr[i - 1] + k, arr[n - 1] - k);
+
+        ans = min(ans, maxHeight - minHeight);
+    }
+
+    return ans;
+        
+    }
+};
