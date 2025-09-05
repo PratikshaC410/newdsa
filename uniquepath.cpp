a4bc0500fc27939d12072d12e1a3437ac5f1d357
Pratiksha Chougule <pratiksha42719@gmail.com>
2025-09-05 10:09:23 +0530

    unique path using bfs

---
+++ uniquepath.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+    vector<int> prev(n, 0);
+
+    for (int i = 0; i < m; i++) {
+        vector<int> curr(n, 0);
+        for (int j = 0; j < n; j++) {
+            if (i == 0 && j == 0)
+                curr[j] = 1;
+            else {
+                int up = (i > 0) ? prev[j] : 0;
+                int left = (j > 0) ? curr[j - 1] : 0;
+                curr[j] = up + left;
+            }
+        }
+        prev = curr;
+    }
+
+    return prev[n - 1];
+}
+
+
+
+    
+};
