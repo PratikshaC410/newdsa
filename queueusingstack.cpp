eac7ec095b0cb01820a967213bb10e51379fab50
Pratiksha Chougule <pratiksha42719@gmail.com>
2025-09-19 18:44:58 +0530

    implementing quue using stack using 2 stacks

---
+++ queueusingstack.cpp
@@ -0,0 +1,43 @@
+
+struct Queue {
+  stack < int > input, output;
+  
+  // Push elements in queue
+  void Push(int data) {
+    // Pop out all elements from the stack input
+    while (!input.empty()) {
+      output.push(input.top());
+      input.pop();
+    }
+    // Insert the desired element in the stack input
+    cout << "The element pushed is " << data << endl;
+    input.push(data);
+    // Pop out elements from the stack output and push them into the stack input
+    while (!output.empty()) {
+      input.push(output.top());
+      output.pop();
+    }
+  }
+  // Pop the element from the Queue
+  int Pop() {
+    if (input.empty()) {
+      cout << "Stack is empty";
+      exit(0);
+    }
+    int val = input.top();
+    input.pop();
+    return val;
+  }
+  // Return the Topmost element from the Queue
+  int Top() {
+    if (input.empty()) {
+      cout << "Stack is empty";
+      exit(0);
+    }
+    return input.top();
+  }
+  // Return the size of the Queue
+  int size() {
+    return input.size();
+  }
+};
