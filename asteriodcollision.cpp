2cc2ea6b0ef51797b32b71ae848269ef519e5d8b
Pratiksha Chougule <pratiksha42719@gmail.com>
2025-09-21 22:19:10 +0530

    asteriod collision

---
+++ asteriodcollision.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
+        for(int a : asteroids){
+            while(!st.empty() and a < 0 and st.top() > 0){
+            int element = st.top();
+                if(element < abs(a)){
+                    st.pop();
+                    continue;
+                }
+                else if(element == abs(a)) st.pop();
+                a = 0;
+                break;
+            }
+            if(a != 0) st.push(a);
+        }
+        vector<int> result(st.size());
+        for(int i = st.size() - 1 ; i >= 0 ; i--){
+            result[i] = st.top();
+            st.pop();
+        }
+        return result;
+    }
+};
