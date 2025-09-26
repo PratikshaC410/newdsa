808ca45597b62bb1bda2fe3860802c850d52bd2f
Pratiksha Chougule <pratiksha42719@gmail.com>
2025-09-26 14:51:39 +0530

    preorder traversal of binary tree

---
+++ preorder.cpp
@@ -0,0 +1,23 @@
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+void preorder(Node* root, vector<int> &arr){
+   
+    if(root == nullptr){
+        return;
+    }
+    arr.push_back(root->data);
+   
+    preorder(root->left, arr);
+    
+    preorder(root->right, arr);
+}
+vector<int> preOrder(Node* root){
+
+    vector<int> arr;
+    preorder(root, arr);
+    return arr;
+}
