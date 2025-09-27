71d009bee7adb799cf03a2b5c33e67519f4ff5a6
Pratiksha Chougule <pratiksha42719@gmail.com>
2025-09-27 22:03:08 +0530

    shortest job first

---
+++ shortjobfirst.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+float shortestJobFirst(vector<int> jobs) {
+   
+    sort(jobs.begin(), jobs.end()); 
+
+    float waitTime = 0; 
+    int totalTime = 0; 
+    int n = jobs.size(); 
+
+    for(int i = 0; i < n; ++i) {
+       
+        waitTime += totalTime; 
+      
+        totalTime += jobs[i]; 
+    }
+    
+    return waitTime / n; 
+}
