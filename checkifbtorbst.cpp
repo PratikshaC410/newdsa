65f8edee0ee33b4cd50c5185ad572184936f9b96
Pratiksha Chougule <pratiksha42719@gmail.com>
2025-10-05 18:14:56 +0530

    check if the givne tree is bt or bst

---
+++ checkifbtorbst.cpp
@@ -0,0 +1,14 @@
+bool isBSTUtil(Node* root, long long minVal, long long maxVal) {
+    if (root == nullptr)
+        return true;
+
+    if (root->data <= minVal || root->data >= maxVal)
+        return false;
+
+    return isBSTUtil(root->left, minVal, root->data) &&
+           isBSTUtil(root->right, root->data, maxVal);
+}
+
+bool isBST(Node* root) {
+    return isBSTUtil(root, LLONG_MIN, LLONG_MAX);
+}
