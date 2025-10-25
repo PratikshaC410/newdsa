71b1a5a1e88d25ec280f106e0ce2b6e58ed0b036
PratikshaC410 <pratiksha42719@gmail.com>
2025-10-25 21:07:52 +0530

    finding the kth largest and kth smallest using maxheap and minheap

---
+++ kthlargestandsmallestusingheap.cpp
@@ -0,0 +1,42 @@
+class Solution {
+
+public:
+
+    void kth_Largest_MaxHeap(vector<int>&arr, int k) {
+
+        priority_queue<int>pq ;
+        int n = arr.size()  ;
+
+        for (int i = 0; i < arr.size(); i++) {
+            pq.push(arr[i])  ;
+        }
+
+        int f = k - 1 ;
+
+        while (f > 0) {
+            pq.pop()  ;
+            f-- ;
+        }
+
+        cout << "Kth Largest element " << pq.top() << "\n"  ;
+    }
+
+    void kth_Smallest_MinHeap(vector<int>&arr, int k) {
+
+        priority_queue<int, vector<int>, greater<int>>pq ;
+        int n = arr.size()  ;
+
+        for (int i = 0; i < arr.size(); i++) {
+            pq.push(arr[i])  ;
+        }
+
+        int f = k - 1 ;
+
+        while (f > 0) {
+            pq.pop()  ;
+            f-- ;
+        }
+
+        cout << "Kth Smallest element " << pq.top() << "\n"  ;
+    }
+} ;
