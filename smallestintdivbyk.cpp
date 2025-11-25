9066770dd31554ff9270215db5a6a612e30e9334
PratikshaC410 <pratiksha42719@gmail.com>
2025-11-25 20:35:13 +0530

    smallest intger divisible by k

---
+++ smallestintdivbyk.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        if (k % 2 == 0 || k % 5 == 0) {
+            return -1;
+        }
+        
+        int remainder = 1 % k;
+        int length = 1;
+
+        while (length <= k) {
+            if (remainder == 0) {
+                return length;
+            }
+            
+            remainder = (remainder * 10 + 1) % k;
+            
+            length++;
+        }
+        
+        return -1;
+    }
+};
