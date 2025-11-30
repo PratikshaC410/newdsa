ee7697b476d29fa5052acffa7b957fd82b055326
PratikshaC410 <pratiksha42719@gmail.com>
2025-11-30 14:02:14 +0530

    max time needed to make the rope colorful such that no two consecutive ropes have same color

---
+++ maxtimetomkeropecolorful.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+           int n = colors.size();
+        int total = 0;
+
+        int i = 0;
+        while (i < n) {
+            char c = colors[i];
+            int sumTime = 0, maxTime = 0;
+            while (i < n && colors[i] == c) {
+                sumTime += neededTime[i];
+                maxTime = max(maxTime, neededTime[i]);
+                i++;
+            }
+
+            total += (sumTime - maxTime);
+        }
+
+        return total;
+    }
+};
