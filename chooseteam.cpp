7c9312b3d572d6f1d9bf628e90e05539eadd740b
PratikshaC410 <pratiksha42719@gmail.com>
2026-01-03 23:00:48 +0530

    greedy approach

---
+++ chooseteam.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n, k;
+    cin >> n >> k;
+
+    int eligible = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int y;
+        cin >> y;
+        if (y <= 5 - k)
+        {
+            eligible++;
+        }
+    }
+
+    cout << eligible / 3 << "\n";
+    return 0;
+}
