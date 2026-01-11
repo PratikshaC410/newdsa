2f4a61dc42f873f85e54d2ed4040774e661c4d36
PratikshaC410 <pratiksha42719@gmail.com>
2026-01-11 21:57:29 +0530

    stock buy and sell

---
+++ maxprofit.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int minPrice = INT_MAX;
+
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.size(); i++)
+        {
+            if (prices[i] < minPrice)
+                minPrice = prices[i];
+
+            int profit = prices[i] - minPrice;
+
+            if (profit > maxProfit)
+                maxProfit = profit;
+        }
+
+        return maxProfit;
+    }
+};
+
+int main()
+{
+    Solution obj;
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+    cout << "Maximum Profit: " << obj.maxProfit(prices) << endl;
+    return 0;
+}
