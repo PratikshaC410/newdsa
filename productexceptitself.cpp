e1624863fc067c3d478c7c29ef099a75799fcca7
PratikshaC410 <pratiksha42719@gmail.com>
2026-01-11 22:05:50 +0530

    finding the product of the array except itself

---
+++ productexceptitself.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> productExceptSelf(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> answer(n, 1);
+
+        // Left product
+        for (int i = 1; i < n; i++)
+        {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        // Right product
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            answer[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return answer;
+    }
+};
