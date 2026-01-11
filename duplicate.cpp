999fa88ef40a19416b0662f8a73d00346d26f384
PratikshaC410 <pratiksha42719@gmail.com>
2026-01-11 22:00:14 +0530

    check if duplicates exist in the array

---
+++ duplicate.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool containsDuplicate(vector<int> nums)
+{
+    unordered_set<int> set;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        set.insert(nums[i]);
+    }
+
+    return set.size() < nums.size();
+}
