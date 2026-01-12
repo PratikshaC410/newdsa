4bbf241afba9bb8b869596ac34034f3c0c9390f4
PratikshaC410 <pratiksha42719@gmail.com>
2026-01-12 21:44:22 +0530

    using dfs to find the leaf nodes

---
+++ cherry.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> deg(n + 1, 0);
+        for (int i = 0; i < n - 1; i++)
+        {
+            int u, v;
+            cin >> u >> v;
+            deg[u]++;
+            deg[v]++;
+        }
+
+        int leaves = 0;
+        for (int i = 2; i <= n; i++)
+        {
+            if (deg[i] == 1)
+                leaves++;
+        }
+
+        if (leaves == 2)
+            cout << "NO\n";
+        else
+            cout << "YES\n";
+    }
+
+    return 0;
+}
