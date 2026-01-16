02b53953e2bfa80ebf9f14e7cc375ebb9643065a
PratikshaC410 <pratiksha42719@gmail.com>
2026-01-16 22:11:24 +0530

    min coins usage greedy approach

---
+++ mincoins.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void solve()
+{
+    long long n;
+    cin >> n;
+    long long c1 = n / 3;
+    long long c2 = n / 3;
+
+    if (n % 3 == 1)
+    {
+        c1++;
+    }
+    else if (n % 3 == 2)
+    {
+        c2++;
+    }
+
+    cout << c1 << " " << c2 << endl;
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
