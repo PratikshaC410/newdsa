61290e2c804dfdfbd05a244f96409088330645c4
PratikshaC410 <pratiksha42719@gmail.com>
2026-02-11 21:22:30 +0530

    basic math implmentation

---
+++ code.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        long long a, b, n;
+        cin >> a >> b >> n;
+
+        int operations = 0;
+
+        while (max(a, b) <= n)
+        {
+            if (a < b)
+            {
+                a += b;
+            }
+            else
+            {
+                b += a;
+            }
+            operations++;
+        }
+
+        cout << operations << endl;
+    }
+
+    return 0;
+}
