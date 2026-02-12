4fcb7598cf49647f8515703e65c726c4da5b14d2
PratikshaC410 <pratiksha42719@gmail.com>
2026-02-12 21:39:07 +0530

    sorting question

---
+++ meeting.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int x1, x2, x3;
+    cin >> x1 >> x2 >> x3;
+
+    int minimum = min({x1, x2, x3});
+    int maximum = max({x1, x2, x3});
+
+    cout << maximum - minimum << endl;
+
+    return 0;
+}
