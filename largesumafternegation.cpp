e89292fff3b0d231634b61b655d5ff1f8f570569
PratikshaC410 <pratiksha42719@gmail.com>
2026-02-19 08:07:40 +0530

    largest sum after doing k negations

---
+++ largesumafternegation.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int largestSumAfterKNegations(vector<int> &nums, int k)
+    {
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() && k > 0; i++)
+        {
+            if (nums[i] < 0)
+            {
+                nums[i] = -nums[i];
+                k--;
+            }
+        }
+
+        int sum = 0;
+        int minAbs = INT_MAX;
+
+        for (int num : nums)
+        {
+            sum += num;
+            minAbs = min(minAbs, abs(num));
+        }
+
+        if (k % 2 == 1)
+        {
+            sum -= 2 * minAbs;
+        }
+
+        return sum;
+    }
+};
