c6fe2f825c55dd087f599fdfa29dba0c795504bb
PratikshaC410 <pratiksha42719@gmail.com>
2026-02-19 23:09:26 +0530

    good array implementation

---
+++ goodarray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[105];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        int operations = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            if ((a[i] % 2) == (a[i - 1] % 2))
+            {
+                operations++;
+            }
+        }
+
+        cout << operations << endl;
+    }
+    return 0;
+}
