36ffbb36dc2e38771cc58bdb3dd69bc87f816c53
PratikshaC410 <pratiksha42719@gmail.com>
2026-02-20 19:25:47 +0530

    pair with the sum less than target

---
+++ pairwithsumlessthantarget.cpp
@@ -0,0 +1,20 @@
+sort(nums.begin(), nums.end());
+
+int left = 0;
+int right = nums.size() - 1;
+int count = 0;
+
+while (left < right)
+{
+    if (nums[left] + nums[right] < target)
+    {
+        count += (right - left);
+        left++;
+    }
+    else
+    {
+        right--;
+    }
+}
+
+return count;
