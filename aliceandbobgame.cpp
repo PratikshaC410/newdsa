771b0eeaae9963ac08807ef0264c51af033b17a7
PratikshaC410 <pratiksha42719@gmail.com>
2026-02-20 22:04:02 +0530

    choose index i and cperform operations

---
+++ aliceandbobgame.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+        }
+
+        if (n % 2 == 1)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
