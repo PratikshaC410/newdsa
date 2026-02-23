ab2e257923e8923f3b65bfb126bfcffc800a5349
PratikshaC410 <pratiksha42719@gmail.com>
2026-02-23 22:28:47 +0530

    marbles of alice updated

--- marblesodalice.cpp
+++ marblesodalice.cpp
@@ -1,39 +1,43 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
+
+void solve()
+{
+    int n;
+    long long a;
+    cin >> n >> a;
+    vector<long long> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    auto it_low = lower_bound(v.begin(), v.end(), a);
+    int count_less = distance(v.begin(), it_low);
+
+    auto it_high = upper_bound(v.begin(), v.end(), a);
+    int count_greater = distance(it_high, v.end());
+
+    if (count_less >= count_greater)
+    {
+        cout << a - 1 << "\n";
+    }
+    else
+    {
+        cout << a + 1 << "\n";
+    }
+}
 
 int main()
 {
-
     int t;
     cin >> t;
-
     while (t--)
     {
-        int n;
-        long long a;
-        cin >> n >> a;
-
-        vector<long long> v(n);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> v[i];
-        }
-
-        long long L = 0, R = 0;
-
-        for (int i = 0; i < n; i++)
-        {
-            if (v[i] < a)
-                L++;
-            else if (v[i] > a)
-                R++;
-        }
-
-        if (L > R)
-            cout << 0 << "\n";
-        else
-            cout << 2000000000 << "\n";
+        solve();
     }
-
     return 0;
 }
