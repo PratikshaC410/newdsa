c5ce4af454707e96cb49e7786ded3c5420c69f24
PratikshaC410 <pratiksha42719@gmail.com>
2026-02-23 22:37:53 +0530

    tab closing updated

--- tabclosing.cpp
+++ tabclosing.cpp
@@ -1,31 +1,26 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-void solve()
-{
-    long long a, b, n;
-    cin >> a >> b >> n;
-
-    long long k = a / b;
-
-    if (n <= k)
-    {
-        cout << n << "\n";
-    }
-    else
-    {
-        cout << k + 1 << "\n";
-    }
-}
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int t;
     cin >> t;
+
     while (t--)
     {
-        solve();
+        long long a, b, n;
+        cin >> a >> b >> n;
+
+        long long k = a / b;
+
+        if (n <= k)
+            cout << n << "\n";
+        else
+            cout << k + 1 << "\n";
     }
+
     return 0;
 }
