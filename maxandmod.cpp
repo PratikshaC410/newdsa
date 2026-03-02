3e71122e0c1fca1ff4fe08f380ec1ce1c975bf8d
PratikshaC410 <pratiksha42719@gmail.com>
2026-03-02 18:22:32 +0530

    max and mod math question

---
+++ maxandmod.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        if (n == 2)
+        {
+            cout << -1 << endl;
+        }
+        else
+        {
+            cout << n << " ";
+            for (int i = 1; i < n; i++)
+            {
+                cout << i << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
